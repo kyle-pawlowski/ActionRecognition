403cb35bc6c1c5eeec5d24dbb105bc85bc328778
kyle-pawlowski <kylepawlowski1@gmail.com>
2020-05-23 23:20:35 -0400

    implemented multitask learning

--- models/temporal_CNN.py
+++ models/temporal_CNN.py
@@ -63,18 +63,24 @@ def temporal_CNN(input_shape, classes, weights_dir, include_top=True):
 
     return model
 
-def dmd_CNN(input_shape, classes, weights_dir, include_top=True):
+def dmd_CNN(input_shape, classes, weights_dir, include_top=True, multitask=False, for_hmdb=False):
     '''
     The CNN for optical flow input.
     Since optical flow is not a common image, we cannot finetune pre-trained ResNet (The weights trained on imagenet is
     for images and thus is meaningless for optical flow)
     :param input_shape: the shape of optical flow input
-    :param classes: number of classes
+    :param classes: number of classes or tuple of (number of ucf classes, number of hmdb classes)
     :return:
     '''
-    optical_flow_input = Input(shape=input_shape)
+    if type(classes) == tuple:
+        ucf_classes = classes[0]
+        hmdb_classes = classes[1]
+    else:
+        ucf_classes = classes
+        
+    dmd_input = Input(shape=input_shape)
 
-    x = Convolution2D(96, kernel_size=(7, 7), strides=(2, 2), padding='same', name='tmp_conv1')(optical_flow_input)
+    x = Convolution2D(96, kernel_size=(7, 7), strides=(2, 2), padding='same', name='tmp_conv1')(dmd_input)
     x = BatchNormalization(axis=3)(x)
     x = Activation('relu')(x)
     x = MaxPooling2D(pool_size=(2, 2))(x)
@@ -99,17 +105,31 @@ def dmd_CNN(input_shape, classes, weights_dir, include_top=True):
     x = Activation('relu')(x)
     x = MaxPooling2D(pool_size=(2, 2),dim_ordering="tf")(x)
 
-    x = Flatten()(x)
-    x = Dense(4096, activation='relu', name='tmp_fc6')(x)
-    x = Dropout(0.5)(x)
+    ucf = Flatten()(x)
+    ucf = Dense(4096, activation='relu', name='tmp_fc6')(ucf)
+    #ucf = Dropout(0.5)(ucf)
+    
+    if multitask:
+        hmdb = Flatten()(x)
+        hmdb = Dense(4096, activation='relu', name='tmp_fc8')(hmdb)
+        #hmdb = Dropout(0.5)(hmdb)
 
-    x = Dense(2048, activation='relu', name='tmp_fc7')(x)
-    x = Dropout(0.5)(x)
+    ucf = Dense(2048, activation='relu', name='tmp_fc7')(ucf)
+    #ucf = Dropout(0.5)(ucf)
+    
+    if multitask:
+        hmdb = Dense(2048, activation='relu', name='tmp_fc9')(hmdb)
+        #hmdb = Dropout(0.5)(hmdb)
 
     if include_top:
-        x = Dense(classes, activation='softmax', name='tmp_fc101')(x)
-
-    model = Model(inputs=optical_flow_input, outputs=x, name='temporal_CNN')
+        ucf = Dense(ucf_classes, activation='softmax', name='tmp_fc101')(ucf)
+        if multitask:
+            hmdb = Dense(hmdb_classes, activation='softmax', name='tmp_fc51')(hmdb)
+            
+    if not multitask or not for_hmdb:
+        model = Model(inputs=dmd_input, outputs=ucf, name='temporal_CNN')
+    else:
+        model = Model(inputs=dmd_input, outputs=hmdb,name='temporal_CNN')
 
     if os.path.exists(weights_dir):
         model.load_weights(weights_dir, by_name=True)
--- train_CNN_dmd.py
+++ train_CNN_dmd.py
@@ -44,7 +44,7 @@ def fit_model(model, train_data, test_data, weights_dir, input_shape, optical_fl
         print(model.summary())
 
         print('Start fitting model')
-        for i in range(5):
+        for i in range(1):
             checkpointer = keras.callbacks.ModelCheckpoint(weights_dir, save_best_only=True, save_weights_only=True)
             earlystopping = keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0.001, patience=20, verbose=2, mode='auto')
             tensorboard = keras.callbacks.TensorBoard(log_dir='./logs/try', histogram_freq=0, write_graph=True, write_images=True)
@@ -57,11 +57,11 @@ def fit_model(model, train_data, test_data, weights_dir, input_shape, optical_fl
                 verbose=2,
                 callbacks=[checkpointer,earlystopping]
             )
-            cwd = os.getcwd()
+            '''cwd = os.getcwd()
             data_dir = os.path.join(cwd,'data')
             list_dir = os.path.join(data_dir, 'ucfTrainTestlist')
             UCF_dir = os.path.join(data_dir, 'UCF-101')
-            regenerate_data(data_dir, list_dir, UCF_dir,temporal='DMD',random=True)
+            regenerate_data(data_dir, list_dir, UCF_dir,temporal='DMD',random=True)'''
 
     except KeyboardInterrupt:
         print('Training is interrupted')
@@ -93,9 +93,9 @@ if __name__ == '__main__':
     #fit_model(model, train_data, test_data, weights_dir, input_shape)
     
     # train CNN using dmd as input
-    dmd_weights_dir = os.path.join(weights_dir, 'dmd_cnn_reshaped.h5')
+    dmd_weights_dir = os.path.join(weights_dir, 'dmd_cnn_multitask.h5')
     video_dir = os.path.join(data_dir, 'DMD_data')
     input_shape = (216,864,6)
     train_data, test_data, class_index = get_data_list(list_dir, video_dir)
-    model = dmd_CNN(input_shape, N_CLASSES, dmd_weights_dir, include_top=True)
+    model = dmd_CNN(input_shape, (N_CLASSES,51), dmd_weights_dir, include_top=True, multitask=True)
     fit_model(model, train_data, test_data, dmd_weights_dir, input_shape, optical_flow=True)
