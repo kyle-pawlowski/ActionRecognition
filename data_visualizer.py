32ef36e1e424e113b4d40c5b03eb596f1e56abfa
kyle-pawlowski <kylepawlowski1@gmail.com>
2020-07-15 20:38:18 -0400

    add data path variable to data visualizer

--- data_visualizer.py
+++ data_visualizer.py
@@ -49,7 +49,7 @@ if __name__ == '__main__':
     of_train_data, of_test_data, class_index = get_data_list(list_dir, of_dir)
     of_gen = sequence_generator(of_train_data,1,of_shape,num_classes)
     '''
-    video = 
+    video = 'train\\ApplyEyeMakeup\\v_ApplyEyeMakeup_g08_c01.npy'
     modes = np.load(os.path.join(dmd_dir, video))
     flow = np.load(os.path.join(of_dir, video))
     #modes = np.reshape(modes, (216,216,4,6))
