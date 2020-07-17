d2b562df2bdb1a0638c7baff9d720e6cb41a3693
kyle-pawlowski <kylepawlowski1@gmail.com>
2020-07-17 19:03:46 -0400

    increase mrdmd dropout

--- models/temporal_CNN.py
+++ models/temporal_CNN.py
@@ -160,10 +160,12 @@ def mrdmd_CNN(input_shape, classes, weights_dir, include_top=True):
     x = Convolution2D(512, kernel_size=(3, 3), strides=(1, 1), padding='same', name='tmp_conv3')(x)
     x = BatchNormalization(axis=3)(x)
     x = Activation('relu')(x)
+    x = Dropout(0.25)(x)
 
     x = Convolution2D(512, kernel_size=(3, 3), strides=(1, 1), padding='same', name='tmp_conv4')(x)
     x = BatchNormalization(axis=3)(x)
     x = Activation('relu')(x)
+    x = Dropout(0.25)(x)
 
     x = Convolution2D(512, kernel_size=(3, 3), strides=(1, 1), padding='same', name='tmp_conv5')(x)
     x = BatchNormalization(axis=3)(x)
@@ -172,10 +174,10 @@ def mrdmd_CNN(input_shape, classes, weights_dir, include_top=True):
 
     x = Flatten()(x)
     x = Dense(4096, activation='relu', name='tmp_fc6')(x)
-    x = Dropout(0.25)(x)
+    x = Dropout(0.5)(x)
 
     x = Dense(2048, activation='relu', name='tmp_fc7')(x)
-    x = Dropout(0.25)(x)
+    x = Dropout(0.5)(x)
 
     if include_top:
         x = Dense(classes, activation='softmax', name='tmp_fc101')(x)
