ccd1d35edde47b0f7bb82a6b590d92d71a4915a0
kyle-pawlowski <kylepawlowski1@gmail.com>
2020-07-21 08:47:36 -0400

    add dropout back in

--- models/temporal_CNN.py
+++ models/temporal_CNN.py
@@ -120,13 +120,13 @@ def dmd_CNN(input_shape, classes, weights_dir, include_top=True, multitask=False
     x = BatchNormalization(axis=3)(x)
     x = Activation('relu')(x)
     if is_training:
-        x = Dropout(0)(x)
+        x = Dropout(0.75)(x)
     
     x = Convolution2D(512, kernel_size=(3, 3), strides=(1, 1), padding='same', name='tmp_conv4')(x)
     x = BatchNormalization(axis=3)(x)
     x = Activation('relu')(x)
     if is_training:
-        x = Dropout(0)(x)
+        x = Dropout(0.75)(x)
 
     x = Convolution2D(512, kernel_size=(3, 3), strides=(1, 1), padding='same', name='tmp_conv5')(x)
     x = BatchNormalization(axis=3)(x)
@@ -138,7 +138,7 @@ def dmd_CNN(input_shape, classes, weights_dir, include_top=True, multitask=False
     ucf_fc6.trainable = not multitask or not for_hmdb
     ucf = ucf_fc6(ucf)
     if is_training: 
-        ucf = Dropout(0.5)(ucf)
+        ucf = Dropout(0.9)(ucf)
     
     if multitask:
         hmdb = Flatten()(x)
@@ -151,7 +151,7 @@ def dmd_CNN(input_shape, classes, weights_dir, include_top=True, multitask=False
     ucf_fc7.trainable = not multitask or not for_hmdb
     ucf = ucf_fc7(ucf)
     if is_training:
-        ucf = Dropout(0.5)(ucf)
+        ucf = Dropout(0.9)(ucf)
     
     if multitask:
         hmdb_fc9 = Dense(2048, activation='relu', name='tmp_fc9')
