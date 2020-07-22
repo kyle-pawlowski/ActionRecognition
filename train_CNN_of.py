ea5fad95e9bea545b25a7e3cf062c29a247bd700
kyle-pawlowski <kylepawlowski1@gmail.com>
2020-07-22 16:43:55 -0400

    fix command line arguements

--- train_CNN_of.py
+++ train_CNN_of.py
@@ -27,5 +27,5 @@ if __name__ == '__main__':
     train_data, test_data, class_index = get_data_list(list_dir, video_dir)
     input_shape = (216, 216, 30)
     model = temporal_CNN(input_shape, (N_CLASSES,51), new_weights_dir, include_top=True,multitask=multitasking)
-    fit_model(model, train_data, test_data, new_weights_dir, input_shape, optical_flow=True)
+    fit_model(model, train_data, test_data, new_weights_dir, input_shape, dataset=dataset, optical_flow=True)
 
